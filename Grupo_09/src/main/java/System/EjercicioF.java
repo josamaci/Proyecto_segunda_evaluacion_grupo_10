02c3778e2350cf103581e4c67940d55fdca55fe6
josamaci <84406998+josamaci@users.noreply.github.com>
2021-12-27 15:21:55 -0500

    arreglando minimax

--- Grupo_09/src/main/java/System/EjercicioF.java
+++ Grupo_09/src/main/java/System/EjercicioF.java
@@ -8,21 +8,18 @@ public class EjercicioF {
         Board b = new Board();
         b.insertChar(0, 2, 'x');
         b.insertChar(0, 1, '0');
-        b.insertChar(0, 0, 'x');
         b.insertChar(2, 2, '0');
         System.out.println(b.getCoordinatesOf('x'));
         System.out.println(b.getCoordinatesOf('0'));
         System.out.println(b);
         b.winOrLose('x','0');
-        System.out.println(b.waysToWin('x'));
-        System.out.println(b.boardUtility('x', '0'));
         
         /*ArrayList<Board> boards = b.generateAlternatives('x');
         for(Board boa:boards){
             System.out.print(boa);
             System.out.println(boa.boardUtility('x', '0'));
         }*/
-       
+        System.out.print(b.generateMinimax('x', 'c'));
         System.out.print(b.minimax('x', 'c'));
     }
 }
--- Grupo_09/src/main/java/TDAs/Tree.java
+++ Grupo_09/src/main/java/TDAs/Tree.java
@@ -1,4 +1,7 @@
 package TDAs;
+
+import java.util.LinkedList;
+
 public class Tree<T> {
     NodeTree<T> root;
 
@@ -9,4 +12,29 @@ public class Tree<T> {
     public Tree() {
         this.root = null;
     }
+
+    public NodeTree<T> getRoot() {
+        return root;
+    }
+
+    public void setRoot(NodeTree<T> root) {
+        this.root = root;
+    }
+    
+    public LinkedList<Tree<T>> getChildren(){
+        return root.getChildren();
+    }
+    
+    public boolean isEmpty(){
+        return this==null || this.root==null;
+    }
+    
+    public boolean isLeaf() {
+        return this.root.getChildren()==null;
+    }
+    
+    public Tree<T> getChild(int index){
+        return this.root.getChildren().get(index);
+    }
+    
 }
