8bf08ec5ce9332f2a1498cd5434d697556dbdb07
josamaci <84406998+josamaci@users.noreply.github.com>
2021-12-28 16:08:02 -0500

    cambios

--- Grupo_09/src/main/java/espol/grupo_09/SettingGameModeController.java
+++ Grupo_09/src/main/java/espol/grupo_09/SettingGameModeController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package espol.grupo_09;
 
 import System.Reader;
@@ -12,16 +7,11 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
 import javafx.scene.control.Button;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.Pane;
-/**
- * FXML Controller class
- *
- * @author JMaci
- */
+
 public class SettingGameModeController implements Initializable {
 
 
@@ -39,12 +29,10 @@ public class SettingGameModeController implements Initializable {
     private ToggleButton btPVE;
     @FXML
     private ToggleButton btPCvsPC;
-    /**
-     * Initializes the controller class.
-     */
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        btSettingChar.setDisable(true);
     }    
     
     @FXML
