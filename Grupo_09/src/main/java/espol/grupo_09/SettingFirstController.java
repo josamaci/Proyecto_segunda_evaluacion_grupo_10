ce6dd280748e6f3d3c902edc1633d1553cd65b4d
codeswax <kevvsole@espol.edu.ec>
2021-12-28 15:58:54 -0500

    archivos css

--- Grupo_09/src/main/java/espol/grupo_09/SettingFirstController.java
+++ Grupo_09/src/main/java/espol/grupo_09/SettingFirstController.java
@@ -9,7 +9,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.Pane;
